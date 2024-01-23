7caee3be25bc43b15e2c32ce891abef7d9cb629d
Mirsaid Abdullaev <abdullaevm017@gmail.com>
2024-01-23 00:00:16 +0000

    Syntax errors fix and printout tidy up.

--- Program.vb
+++ Program.vb
@@ -15,12 +15,13 @@ Module Program
         Try
             MessageLen = Console.ReadLine()
             If MessageLen > 100 Or MessageLen < 10 Then
-                Message = StrDup(2, "abcdefghijklmnopqrstuvwxyz") 'len 56
+                Throw New Exception
             Else
                 Message = StrDup(MessageLen, "X")
             End If
         Catch
             Message = StrDup(2, "abcdefghijklmnopqrstuvwxyz") 'len 56
+            Console.WriteLine("Error parsing input: Message set to default value of length 56.")
         End Try
         ThreadCount = 4
         Console.WriteLine(vbCrLf & "Enter DOS mode: press '1' on the keyboard")
@@ -44,7 +45,7 @@ Module Program
                 DataTransferred = Math.Round(CInt(Count) * Message.Length / 1000000, 5)
                 Duration = Now - StartTime
                 Console.WriteLine(vbCrLf & vbCrLf & $"Successfully exited program. Packets sent:  {Count}, Data total: {DataTransferred} MB. Duration: {Duration.TotalSeconds} seconds.")
-                Console.WriteLine(vbCrLf & "THANKS FOR USING THE DOS SIMULATOR! :)" & vbCrLf)
+                Console.WriteLine(vbCrLf & "THANKS FOR USING THE DOS SIMULATOR! Click any key to exit :)" & vbCrLf)
                 Console.WriteLine("k:lkXWWWMMMMMMMWWNXXXXNWWMMMMMMMMWNKxcc0" & vbCrLf & "d...,cdkO0K0Odc:;,,;;,;;:ldOKK0Oxo:'..'k" & vbCrLf & "x'.........'',;:clllllc::;,''.........;0" & vbCrLf & "K:........,:looooooooooooool:,........lN" & vbCrLf & "WO,.....,cllllllllllllllllllll:'.....cKM" & vbCrLf & "MW0:...,:cccclllllllllllllclccc:....lXWM" & vbCrLf & "MMNl...,:::cccccccccccccccc:cc::'...xWMM" & vbCrLf & "MWk'...',;;;;::::cccccc::::;;;;,....;KMM" & vbCrLf & "MNl...'',;,...':loloooll;'..',;,'....xWM" & vbCrLf & "MXc..',;:cccc'  ,oxdxdl. .,ccc::;,...dWM" & vbCrLf & "MNl..';:clldl.  'dkkkko.  'ooolc:;'..xWM" & vbCrLf & "MWx..';:lloxo' .lOOkkOk:. ;ddolc:;'.,0MM" & vbCrLf & "MMXc.';clododxodOOOOOOOkddxdodol:,..dNMM" & vbCrLf & "MMW0:.,:codo;;coxkOkkkkdo:;codoc;''lXMMM" & vbCrLf & "MMMWKl,,:loddl;''......',:oxdoc;',dXWWMM" & vbCrLf & "MMMMWXkc,;:lodkxdolllloxkxdoc:,,lONMMMMM" & vbCrLf & "MMMMMWWXkl:;;:clooddddoolc:;;:oONWMMMMMM" & vbCrLf & "MMMMMMMMWNKkdlc::::::::::cldOKWWMMMMMMMM" & vbCrLf & "MMMMMMMMMMMWWNXK0OOkkOO0KXNWWWMMMMMMMMMM" & vbCrLf & "MMMMMMMMMMMMMMMWWWWWWWWWWWMMMMMMMMMMMMMM")
                 AppRunning = False
             End If
@@ -54,9 +55,9 @@ Module Program
         RunningDOS = True
         StartTime = Now
         Dim TempLst As New List(Of UInteger)
-        Dim Client As New Sockets.UdpClient(18000) With {.EnableBroadcast = True}
         For i As UInteger = 0 To ThreadCount - 1
             TempLst.Add(30000 + 15 * i)
+            Dim Client As New Sockets.UdpClient(CInt(18000 + 15 * i)) With {.EnableBroadcast = True}
             Dim DestEP As New IPEndPoint(IPAddress.Broadcast, 30000 + 15 * i)
             ThreadPool.QueueUserWorkItem(Sub() BroadcastData(Client, DestEP), i)
         Next
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NetSimulator
+# NetSimulator - a small-scale LAN DOS tool in VB.NET, written by Mirsaid Abdullaev
+<ul>Use only with prior permission from LAN admins</ul>
+<ul>Coded in VB.NET using Visual Studio 2022</ul>
+<ul>Feel free to submit PR's for any unattended bugs or oversights :)</ul>
+<ul>Potential future improvements - threading. Find a way to run many concurrent broadcasters in a way that allows even more throughput</ul>
